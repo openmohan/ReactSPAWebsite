3cff929a462cc28b9858bd67ffe0049d6066a75d
mohanprasaths <mohanprasath@live.com>
2017-02-16 14:57:09 +0530

    added layout to the application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import styles from './app.css';
+import LayoutPage from './components/LayoutPage.jsx'
 
 export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { count: 0 };
-  }
-
-  increment() {
-    this.setState({
-      count: this.state.count + 1
-    });
-  }
-
   render() {
     return (
       <div className={styles.app}>
-        <p>{ this.state.count }</p>
-        <button onClick={() => this.increment()}>Increments</button>
+          <LayoutPage/>
       </div>
     )
   }
---
+++ src/components/LayoutPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import FacebookLogin from 'react-facebook-login';
+var LayoutPage = React.createClass({
+	render : function(){
+	return(
+	<div className="container-fluid height100">
+	 <div className="row height100 no-gutter">
+	  <div className="col-md-3 SidePageColor height100"></div>
+	   <div className="col-md-6 MainPageColor height100">
+	   {this.props.children && React.cloneElement(this.props.children,{data: this.props , dispatch:this.props.dispatch  }) }
+	   </div>
+	  <div className="col-md-3 height100 SidePageColor"></div>	
+	 </div>
+	</div>
+	
+	)}
+})
+
+export default LayoutPage
